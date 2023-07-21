2a3cde8ac8fe3b81c08819f5e1bb3b46a2196ef3
zuwuko <marcopana@gmail.com>
2023-07-21 12:36:54 -0500

    triming down the codebase by eliminating services not related to watcher

--- services/services.go
+++ services/services.go
@@ -5,26 +5,12 @@ import (
 )
 
 var STATE *models.State
-var SCANNER *Scanner
-var PARSER *Parser
 
 func StartServices() (err error) {
 	STATE, err = BootstrapState()
 	if err != nil {
 		panic(err)
 	}
-
-	//SCANNER, err = NewScanner()
-	//if err != nil {
-	//	return err
-	//}
-	//SCANNER.Run()
-
-	//PARSER, err := NewParser()
-	//if err != nil {
-	//	return err
-	//}
-	//PARSER.Run()
 
 	TxWatcher, err := NewTxWatcher()
 	if err != nil {
