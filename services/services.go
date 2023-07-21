5094a150966911c2abd01f07b20bfe5620d73d8e
zuwuko <marcopana@gmail.com>
2023-07-21 13:21:43 -0500

    Separate the ntc-services into their perspective implementations (#12)
    * preparing yml's for prod migration
    
    * triming down the codebase by eliminating services not related to watcher
    
    * Remove unneeded model and depoyment artifacts
    
    * This will run the deploy on CircleCI for dev environment
    
    * Correct indentation in circleci configuration yml
    
    ---------
    
    Co-authored-by: berryhill <matthewberryhill@gmail.com>

--- services/services.go
+++ services/services.go
@@ -5,8 +5,6 @@ import (
 )
 
 var STATE *models.State
-var SCANNER *Scanner
-var PARSER *Parser
 
 func StartServices() (err error) {
 	STATE, err = BootstrapState()
@@ -14,25 +12,11 @@ func StartServices() (err error) {
 		panic(err)
 	}
 
-	//SCANNER, err = NewScanner()
-	//if err != nil {
-	//	return err
-	//}
-	//SCANNER.Run()
-
-	//PARSER, err := NewParser()
-	//if err != nil {
-	//	return err
-	//}
-	//PARSER.Run()
-
 	TxWatcher, err := NewTxWatcher()
 	if err != nil {
 		panic(err)
 	}
 	go TxWatcher.Run()
-
-	//Inscribe()
 
 	return nil
 }
